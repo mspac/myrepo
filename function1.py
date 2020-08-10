3acf7be5611d4ae2f01702fd5af9a3283f3a8cb7
mspace <68471211+mspac@users.noreply.github.com>
2020-08-10 14:59:31 +0530

    Delete function1.py

--- function1.py
+++
@@ -1,8 +0,0 @@
-
-#defining the function
-def greeting():
-    print('welcome to green place')
-
-#calling the function
-greeting()   #1time calling so -- 1 time execution
-greeting()   #again calling so -- agian execution
