c88287963358ddfd4ef73a5a01ea01b32e1f80f5
mspac <work.mspace@gmail.com>
2020-08-10 15:02:17 +0530

    function examples

---
+++ function1.py
@@ -0,0 +1,8 @@
+
+#defining the function
+def greeting():
+    print('welcome to green place')
+
+#calling the function
+greeting()   #1time calling so -- 1 time execution
+greeting()   #again calling so -- agian execution
